67740e024267af9d5d9a1a0aa83d48800fc11ac7
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-22 18:03:45 +0530

    add Procfile for deployment

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
@@ -40,5 +41,6 @@ def contact():
 
 
 if __name__ == "__main__":
-
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
+    app.run(debug=False)
