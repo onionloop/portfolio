98bba004a68eb10e78a0ad0beee04ddc596347b5
onionloop <125755906+onionloop@users.noreply.github.com>
2026-03-22 17:27:12 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route('/')
+
+def home():
+
+    data ={
+        'name': 'Aditya More',
+        'title': 'Python Developer & CS Student.',
+        'about': 'I build cool stuff with Python',
+        'skills': ['Python', 'C++', 'HTML/CSS', 'Flask', 'Linux'],
+        'projects': [
+            {
+                'title': 'Portfolio Website',
+                'desc': 'Personal site built with Flask and HTML/CSS',
+                'tech': 'Python, Flask, HTML/CSS'
+
+            },
+
+            {
+                'title': 'Coming Soon',
+                'desc': 'Next project loading soon with more Flask and frontend...',
+                'tech': 'Python, Flask, HTML/CSS'
+            }
+        ]
+    
+    }
+    return render_template('index.html', **data)
+
+
+@app.route('/contact', methods=['POST'])
+def contact():
+    
+    name = request.form['name']
+    email = request.form['email']
+    message = request.form['message']
+    print(f"Message from {name} [{email}]: {message}")
+    return '<h2> Message received! Go back <a href="/">Home</a></h2>'
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
